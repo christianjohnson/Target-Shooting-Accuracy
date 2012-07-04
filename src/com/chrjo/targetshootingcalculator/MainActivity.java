3bb8b654234de275909a49f96e3955709909b9ad
Christian Johnson <christian@christianjohnson.org>
2012-07-03 20:26:51 -0700

    Project

---
+++ src/com/chrjo/targetshootingcalculator/MainActivity.java
@@ -0,0 +1,12 @@
+package com.chrjo.targetshootingcalculator;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
