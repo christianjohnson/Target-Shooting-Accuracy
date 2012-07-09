17e205f951167545a477aaa67bf33ea4711159a4
Dan <jewishdan18@gmail.com>
2012-07-09 00:23:10 -0700

    Labels

--- assets/www/js/calculate_stats.js
+++ assets/www/js/calculate_stats.js
@@ -19,6 +19,33 @@ function avg_dist(C,L)
     return s/L.length;
 }
 
+function get_label(d)
+{
+    if(d<0) {
+        return "perfect";
+    } else if(d < .1) {
+        return "perfect";
+    } else if(d < .2) {
+        return "excellent";
+    } else if(d < .3) {
+        return "very good";
+    } else if(d < .4) {
+        return "good";
+    } else if(d < .5) {
+        return "okay";
+    } else if(d < .6) {
+        return "meh";
+    } else if(d < .7) {
+        return "poor";
+    } else if(d < .8) {
+        return "bad";
+    } else if(d < .9) {
+        return "dreadful";
+    } else {
+        return "you shouldn't shoot while drinking";
+    }
+}
+
 function dist_distribution(c, pointList, X, Y) 
 {
     var m = new Object();
@@ -29,7 +56,7 @@ function dist_distribution(c, pointList, X, Y)
         if(m[d] == undefined)
         {
             m[d] = new Array();
-            m[d][0] = String(d);
+            m[d][0] = get_label(d);
             m[d][1] = 0;
         }
         m[d][1] += 1;
